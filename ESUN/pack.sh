d6fac80c0cdc1909b874467d9a1f2fe33e17d504
allen.an <allen.an@chinasystems.com>
2018-09-07 17:28:49 +0800

    esun

--- ESUN/pack.sh
+++ ESUN/pack.sh
@@ -3,12 +3,25 @@
 function list(){
     cat ./oldFile/list.txt | while read line
     do
-        echo ${line#*/}
-        echo ${line##*/}
+        if [[ ${line} =~ "/src/" ]];then
+           Ajar ${line}
+            continue
+        elif [[ ${line} =~ "/WebContent/" ]];then
+            echo "js ${line}"
+            continue
+        elif [[ ${line} =~ "jar" ]];then
+            echo "jar ${line}"
+            continue
+        fi
     done
-
 }
 
+function Ajar(){
+    str=${1//src/}
+    pName=${str[0]}
+    jName=${str[1]%.*}
+    echo ${pName}
+}
 
 echo "pls put mv old verson into oldFile folder "
 echo "eeps"
---
+++ ESUN/test.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+a="/CSBase/src/com/cs/base/utility/uuid/UUIDGenerator.java"
+
+if [[ $a =~ "src" ]];then
+    echo "11"
+else
+    echo "22"
+fi
